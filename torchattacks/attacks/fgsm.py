88123f937107b4a9f44356af01948ed7fe73b02a
HanfeiBu <hanfei.bu@cooper.edu>
2024-06-24 17:36:11 -0700

    Update fgsm.py
    preliminary change, test later

--- torchattacks/attacks/fgsm.py
+++ torchattacks/attacks/fgsm.py
@@ -53,12 +53,24 @@ class FGSM(Attack):
         else:
             cost = loss(outputs, labels)
 
-        # Update adversarial images
-        grad = torch.autograd.grad(
-            cost, images, retain_graph=False, create_graph=False
-        )[0]
+        c = 1e-3, k = 1
+        for i in range(k):
+            noise = torch.randn_like(data)
+            new_data = c*noise+data
 
-        adv_images = images + self.eps * grad.sign()
+            output2 = model(new_data)
+            
+            loss2 = F.nll_loss(output2, label)
+            # changed loss to cost
+            data_grad += (noise*(loss2-cost)/c)/k
+        
+        # Update adversarial images, remove? test later
+        # grad = torch.autograd.grad(
+        #     cost, images, retain_graph=False, create_graph=False
+        # )[0]
+
+        # changed grad here to data_grad
+        adv_images = images + self.eps * data_grad.sign()
         adv_images = torch.clamp(adv_images, min=0, max=1).detach()
 
         return adv_images
